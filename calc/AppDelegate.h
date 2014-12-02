1045ac06edb1bf4281f0bae562a0b9659673f73c
伊藤喬一郎 <nagatomo1218@gmail.com>
2014-12-02 11:51:29 +0900

    Initial Commit

---
+++ calc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  calc
+//
+//  Created by 伊藤喬一郎 on 2014/12/02.
+//  Copyright (c) 2014年 伊藤喬一郎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ calc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  calc
+//
+//  Created by 伊藤喬一郎 on 2014/12/02.
+//  Copyright (c) 2014年 伊藤喬一郎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
